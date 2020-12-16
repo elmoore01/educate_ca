2fb6fedcd969b2e8c0e5989e0b3ab7df3c4f36db
Eric Moore <eric@educate-ca.org>
2020-12-16 13:06:18 -0800

    first commit

---
+++ main_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path
+from django.contrib import admin
+
+from . import  views
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('forum', views.forum_page, name='forum'),
+    path('contact', views.contact_page, name='contact'),
+    path('lifeprep', views.lifeprep_page, name='lifeprep'),
+    path('partners', views.partners_page, name='partners'),
+    path('board', views.board_page, name='board'),
+    path('financials', views.financials_page, name='financials'),
+    path('donate', views.donate_page, name='donate'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home_page(request):
+    return render(request, 'main_app/home.html')
+
+def forum_page(request):
+    return render(request, 'main_app/forum.html')
+
+def lifeprep_page(request):
+    return render(request, 'main_app/lifeprep.html')
+
+def contact_page(request):
+    return render(request, 'main_app/contact.html')
+
+def partners_page(request):
+    return render(request, 'main_app/partners.html')
+
+def board_page(request):
+    return render(request, 'main_app/board.html')
+
+def financials_page(request):
+    return render(request, 'main_app/financials.html')
+
+def donate_page(request):
+    return render(request, 'main_app/donate.html')
